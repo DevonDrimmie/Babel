7eb0b1aa0385d4095d2c8855e2fb5f55f04e189f
Devon Miller <devon@Devons-MacBook-Air.local>
2018-10-26 16:03:41 -0400

    Working mvp

---
+++ index.js
@@ -0,0 +1,184 @@
+const functions = require('firebase-functions');
+const { dialogflow } = require('actions-on-google');
+const translate2 = require('google-translate-api');
+const {Translate} = require('@google-cloud/translate');
+
+// Your Google Cloud Platform project ID
+const projectId = 'babel-6985a';
+
+// Instantiates a client
+const translate = new Translate({
+  projectId: projectId,
+});
+
+
+
+const app = dialogflow();
+
+var langs = {
+    'af': 'Afrikaans',
+    'sq': 'Albanian',
+    'am': 'Amharic',
+    'ar': 'Arabic',
+    'hy': 'Armenian',
+    'az': 'Azerbaijani',
+    'eu': 'Basque',
+    'be': 'Belarusian',
+    'bn': 'Bengali',
+    'bs': 'Bosnian',
+    'bg': 'Bulgarian',
+    'ca': 'Catalan',
+    'ceb': 'Cebuano',
+    'ny': 'Chichewa',
+    'zh-cn': 'Chinese Simplified',
+    'zh-tw': 'Chinese Traditional',
+    'co': 'Corsican',
+    'hr': 'Croatian',
+    'cs': 'Czech',
+    'da': 'Danish',
+    'nl': 'Dutch',
+    'en': 'English',
+    'eo': 'Esperanto',
+    'et': 'Estonian',
+    'tl': 'Filipino',
+    'fi': 'Finnish',
+    'fr': 'French',
+    'fy': 'Frisian',
+    'gl': 'Galician',
+    'ka': 'Georgian',
+    'de': 'German',
+    'el': 'Greek',
+    'gu': 'Gujarati',
+    'ht': 'Haitian Creole',
+    'ha': 'Hausa',
+    'haw': 'Hawaiian',
+    'iw': 'Hebrew',
+    'hi': 'Hindi',
+    'hmn': 'Hmong',
+    'hu': 'Hungarian',
+    'is': 'Icelandic',
+    'ig': 'Igbo',
+    'id': 'Indonesian',
+    'ga': 'Irish',
+    'it': 'Italian',
+    'ja': 'Japanese',
+    'jw': 'Javanese',
+    'kn': 'Kannada',
+    'kk': 'Kazakh',
+    'km': 'Khmer',
+    'ko': 'Korean',
+    'ku': 'Kurdish (Kurmanji)',
+    'ky': 'Kyrgyz',
+    'lo': 'Lao',
+    'la': 'Latin',
+    'lv': 'Latvian',
+    'lt': 'Lithuanian',
+    'lb': 'Luxembourgish',
+    'mk': 'Macedonian',
+    'mg': 'Malagasy',
+    'ms': 'Malay',
+    'ml': 'Malayalam',
+    'mt': 'Maltese',
+    'mi': 'Maori',
+    'mr': 'Marathi',
+    'mn': 'Mongolian',
+    'my': 'Myanmar (Burmese)',
+    'ne': 'Nepali',
+    'no': 'Norwegian',
+    'ps': 'Pashto',
+    'fa': 'Persian',
+    'pl': 'Polish',
+    'pt': 'Portuguese',
+    'ma': 'Punjabi',
+    'ro': 'Romanian',
+    'ru': 'Russian',
+    'sm': 'Samoan',
+    'gd': 'Scots Gaelic',
+    'sr': 'Serbian',
+    'st': 'Sesotho',
+    'sn': 'Shona',
+    'sd': 'Sindhi',
+    'si': 'Sinhala',
+    'sk': 'Slovak',
+    'sl': 'Slovenian',
+    'so': 'Somali',
+    'es': 'Spanish',
+    'su': 'Sundanese',
+    'sw': 'Swahili',
+    'sv': 'Swedish',
+    'tg': 'Tajik',
+    'ta': 'Tamil',
+    'te': 'Telugu',
+    'th': 'Thai',
+    'tr': 'Turkish',
+    'uk': 'Ukrainian',
+    'ur': 'Urdu',
+    'uz': 'Uzbek',
+    'vi': 'Vietnamese',
+    'cy': 'Welsh',
+    'xh': 'Xhosa',
+    'yi': 'Yiddish',
+    'yo': 'Yoruba',
+    'zu': 'Zulu'
+};
+
+function isSupported(desiredLang) {
+    return Boolean(getCode(desiredLang));
+}
+
+function getCode(desiredLang) {
+    if (!desiredLang) {
+        return false;
+    }
+    desiredLang = desiredLang.toLowerCase();
+
+    if (langs[desiredLang]) {
+        return desiredLang;
+    }
+
+    var keys = Object.keys(langs).filter(function (key) {
+        if (typeof langs[key] !== 'string') {
+            return false;
+        }
+
+        return langs[key].toLowerCase() === desiredLang;
+    });
+
+    return keys[0] || false;
+}
+
+
+app.intent('outLang', (conv, params) => {
+    conv.data.session = {};
+    let session = conv.data.session;
+    let outLang = params.outLang;
+    if(isSupported(outLang)){
+        session.outLang = getCode(outLang);
+        conv.ask("Got it! translation begins now, please say exit to end conversation.");
+    }
+    else{
+        conv.close("Im Sorry, that language is not supported by this application. Goodbye.");
+    }  
+});
+
+app.intent('translateInput', (conv, params) => {
+    let session = conv.data.session;
+    let input = params.userInput;
+
+    console.log("UPDATED LOGS: ", input, session.inLang, session.outLang);
+
+    return translate
+        .translate(input, session.outLang)
+        .then(results => {
+            console.log("Made it here");
+            const output = results[0];
+            conv.ask(output)
+        })
+        .catch(err => {
+            conv.close('Error translating your message, please try again later.')
+            console.error('my ERROR:', err);
+        });
+    
+});
+
+exports.babel = functions.https.onRequest(app);
